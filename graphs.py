53fb9f5b6a71f888dfbf887116d9e09eb60352b7
Sophia Lin <50805131+sophialin7@users.noreply.github.com>
2023-05-26 11:50:11 -0700

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -6,16 +6,29 @@ files = ['num_posts_and_sentiment_summary_2012.csv', 'num_posts_and_sentiment_su
          'num_posts_and_sentiment_summary_2015.csv', 'num_posts_and_sentiment_summary_2016.csv', 'num_posts_and_sentiment_summary_2017.csv', 
          'num_posts_and_sentiment_summary_2018.csv', 'num_posts_and_sentiment_summary_2019.csv', 'num_posts_and_sentiment_summary_2020.csv', 
          'num_posts_and_sentiment_summary_2021.csv', 'num_posts_and_sentiment_summary_2022.csv']
+
 df = pd.DataFrame()
+
+# merging all the files together
 for file in files:
     data = pd.read_csv(file)
     df = pd.concat([df, data], axis=0)
 df.to_csv('merged_files.csv', index=False)
 merged_data = pd.read_csv('merged_files.csv')
 
+# change date to just year
+# merged_data["DATE"] = pd.to_datetime(merged_data["DATE"])
+# merged_data["YEAR"] = merged_data["DATE"].dt.year
+
 # interactive line plot
 fig = px.line(merged_data, x='DATE', y='SCORE', title='Sentiment Score')
 
-fig.update_layout(xaxis_title='Year', yaxis_title='Sentiment Score')
+fig.update_layout(xaxis_title='Date', yaxis_title='Sentiment Score')
 
 pio.write_image(fig, 'sentiment_score.png')
+
+fig2 = px.line(merged_data, x='DATE', y='N', title='Daily Number of Posts')
+
+fig2.update_layout(xaxis_title='Date', yaxis_title='Posts')
+
+pio.write_image(fig2, 'posts.png')
